55eb8dafaf566ff2270ff5a38eac783ef0af5ea2
jeremy-22 <jeryrangmart@gmail.com>
2024-02-06 23:55:40 -0600

    implemente el optimazador SGD_momentum a la red, en el renglon 14, en donde agrege el parametro momentum y en el renglon 32, donde actualize el minibatch con momentum

--- Network_2.py
+++ Network_2.py
@@ -11,38 +11,40 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             a = Network.sigmoid(np.dot(w, a)+b)
         return a
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
+    def SGD_momentum(self, training_data, epochs, mini_batch_size, eta, momentum,
             test_data=None):
         training_data = list(training_data)
-        
-        n = len(training_data)  
+        n = len(training_data)
         if test_data:
             test_data = list(test_data)
             n_test = len(test_data) 
-
         for j in range(epochs):
-            random.shuffle(training_data) 
+            random.shuffle(training_data)
             mini_batches = [
                 training_data[k:k+mini_batch_size]
-                for k in range(0, n, mini_batch_size)] 
+                for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.update_mini_batch_momentum(mini_batch, eta, momentum)
             if test_data:
                 print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
             else:
                 print("Epoch {} complete".format(j))
-          
-    def update_mini_batch(self, mini_batch, eta): 
+    def update_mini_batch_momentum(self, mini_batch, eta, momentum):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch: 
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y) 
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw 
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb 
-                       for b, nb in zip(self.biases, nabla_b)]
+        v_w = [np.zeros(w.shape) for w in self.weights]
+        v_b = [np.zeros(b.shape) for b in self.biases]
+
+        v_b = [momentum * vb - nb
+                      for vb, nb in zip(v_b, nabla_b)]
+        v_w = [momentum * vw - nw
+                      for vw, nw in zip(v_w, nabla_w)]
+        self.biases = [b + (eta / len(mini_batch))*vb for b, vb in zip(self.biases, v_b)]
+        self.weights = [w + (eta / len(mini_batch))*vw for w, vw in zip(self.weights, v_w)]
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
--- ejemplo.py
+++ ejemplo.py
@@ -7,9 +7,9 @@ training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
 
-net=Network_2.Network([784, 30, 10]) # CAMBIÉ EL NOMBRE DEL ARCHIVO NETWORK
+net=Network_2.Network([784, 30, 10]) 
 #net.SG_momentum( training_data, 30, 10, 3.0,0.91, test_data=test_data)
-net.SGD(training_data, 10, 10 ,3,test_data=test_data)
+net.SGD_momentum(training_data, 30, 10 ,3, 0.91, test_data=test_data)
 #training_data, epochs, mini_batch_size, eta, momentum,test_data=None)
 archivo = open("red_prueba.pkl",'wb')
 pickle.dump(net,archivo)
